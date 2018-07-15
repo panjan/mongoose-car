4334ef3cea57f5080a42b8f3dce0971253d6c8d5
Jan Zdráhal <hi@honza.me>
2018-07-16 01:49:53 +0200

    Add basic browser client

---
+++ fs/script.js
@@ -0,0 +1,56 @@
+var sock = new WebSocket(`ws://${document.location.href}rpc`)
+
+sock.onmessage = function(event) {
+  console.log(event.data)
+}
+
+function sendRPC(method, params) {
+  data = {
+    "jsonrpc": "2.0",
+    "method": method,
+    "params": params
+  }
+  sock.send(JSON.stringify(data))
+}
+
+document.onkeydown = function(e) {
+  switch (e.keyCode) {
+    case 37:
+      console.log('left');
+      sendRPC('left')
+      break;
+    case 38:
+      console.log('up');
+      sendRPC('forward')
+      break;
+    case 39:
+      console.log('right');
+      sendRPC('right')
+      break;
+    case 40:
+      console.log('down');
+      sendRPC('backward')
+      break;
+  }
+};
+
+document.onkeyup = function(e) {
+  switch (e.keyCode) {
+    case 37:
+      console.log('stop');
+      sendRPC('stop')
+      break;
+    case 38:
+      console.log('stop');
+      sendRPC('stop')
+      break;
+    case 39:
+      console.log('stop');
+      sendRPC('stop')
+      break;
+    case 40:
+      console.log('stop');
+      sendRPC('stop')
+      break;
+  }
+};
